da71bad66c0888d5fd6dd40572fbf9fd87811271
= <=>
2021-12-21 00:11:17 +0530

    acer api added

--- acer-api/src/main/java/com/example/activities/controller/JwtAuthenticationRestController.java
+++ acer-api/src/main/java/com/example/activities/controller/JwtAuthenticationRestController.java
@@ -48,7 +48,8 @@ public class JwtAuthenticationRestController {
     @RequestMapping(value = "${jwt.get.token.uri}", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtTokenRequest authenticationRequest)
             throws AuthenticationException {
-        this.authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        this.
+                authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
         final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new JwtTokenResponse(token));

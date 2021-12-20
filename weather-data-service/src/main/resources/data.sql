e6af6b8bdd2a1d289a2ce35c53566f5d967dad5d
= <=>
2021-12-20 22:54:15 +0530

    Microservices addition

---
+++ weather-data-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into weather_data
+(id,city,temperature,environment)
+values(10001,'New York',11,'');
+insert into weather_data
+(id,city,temperature,environment)
+values(10002,'Sharjah',35,'');
+insert into weather_data
+(id,city,temperature,environment)
+values(10003,'Geneva',4,'');
